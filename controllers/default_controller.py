1bcf831c8a3f015a3b7b9924b579cc5c8c038327
Bill Gillock <gillock1@slb.com>
2016-05-27 12:33:31 +0200

    make work with 2.7

--- controllers/default_controller.py
+++ controllers/default_controller.py
@@ -1,18 +1,18 @@
 
-def surface_surface_id_geo_hash_property_id_image_get(surfaceId, geoHash, propertyId, colorMap) -> str:
+def surface_surface_id_geo_hash_property_id_image_get(surfaceId, geoHash, propertyId, colorMap):
     return 'do some magic!'
 
-def surface_surface_id_geo_hash_properties_get(surfaceId, geoHash) -> str:
+def surface_surface_id_geo_hash_properties_get(surfaceId, geoHash):
     return 'do some magic!'
 
-def surface_surface_id_get(surfaceId) -> str:
+def surface_surface_id_get(surfaceId):
     return 'do some magic!'
 
-def surfaces_get(domain) -> str:
+def surfaces_get(domain):
     return 'do some magic!'
 
-def surface_surface_id_geo_hash_triangles_get(surfaceId, geoHash) -> str:
+def surface_surface_id_geo_hash_triangles_get(surfaceId, geoHash):
     return 'do some magic!'
 
-def surface_surface_id_geo_hash_vertices_get(surfaceId, geoHash) -> str:
+def surface_surface_id_geo_hash_vertices_get(surfaceId, geoHash):
     return 'do some magic!'
