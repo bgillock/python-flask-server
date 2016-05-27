768d25dcba610f240ba5b38ef45ac7ee9e07c08a
Bill Gillock <gillock1@slb.com>
2016-05-27 09:25:19 +0200

    initial

---
+++ controllers/default_controller.py
@@ -0,0 +1,18 @@
+
+def surface_surface_id_geo_hash_property_id_image_get(surfaceId, geoHash, propertyId, colorMap) -> str:
+    return 'do some magic!'
+
+def surface_surface_id_geo_hash_properties_get(surfaceId, geoHash) -> str:
+    return 'do some magic!'
+
+def surface_surface_id_get(surfaceId) -> str:
+    return 'do some magic!'
+
+def surfaces_get(domain) -> str:
+    return 'do some magic!'
+
+def surface_surface_id_geo_hash_triangles_get(surfaceId, geoHash) -> str:
+    return 'do some magic!'
+
+def surface_surface_id_geo_hash_vertices_get(surfaceId, geoHash) -> str:
+    return 'do some magic!'
